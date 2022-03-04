4726c69e498bd6d97f555730214b515b21e698f3
kierstenv <kierstenvicknair22@gmail.com>
2022-03-04 04:10:03 -0600

    audit tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,16 +3,13 @@ var tasks = {};
 var createTask = function(taskText, taskDate, taskList) {
   // create elements that make up a task item
   var taskLi = $("<li>").addClass("list-group-item");
-  var taskSpan = $("<span>")
-    .addClass("badge badge-primary badge-pill")
-    .text(taskDate);
-  var taskP = $("<p>")
-    .addClass("m-1")
-    .text(taskText);
+  var taskSpan = $("<span>").addClass("badge badge-primary badge-pill").text(taskDate);
+  var taskP = $("<p>").addClass("m-1").text(taskText);
 
   // append span and p element to parent li
   taskLi.append(taskSpan, taskP);
 
+  auditTask(taskLi);
 
   // append to ul list on the page
   $("#list-" + taskList).append(taskLi);
@@ -45,7 +42,19 @@ var saveTasks = function() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 };
 
+const auditTask = (taskEl) => {
+  const date = $(taskEl).find("span").text().trim();
 
+  const time = moment(date, "L").set("hour", 17);
+  
+  $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
+
+  if (moment().isAfter(time)) {
+    $(taskEl).addClass("list-group-item-danger");
+  } else if (Math.abs(moment().diff(time, "days")) <= 2) {
+    $(taskEl).addClass("list-group-item-warning");
+  }
+};
 
 // modal was triggered
 $("#task-form-modal").on("show.bs.modal", function() {
@@ -57,6 +66,11 @@ $("#task-form-modal").on("show.bs.modal", function() {
 $("#task-form-modal").on("shown.bs.modal", function() {
   // highlight textarea
   $("#modalTaskDescription").trigger("focus");
+});
+
+// date picker
+$("#modalDueDate").datepicker({
+  minDate: 1
 });
 
 // save button in modal was clicked
@@ -83,53 +97,42 @@ $("#task-form-modal .btn-primary").click(function() {
 
 // edit task d8s
 $(".list-group").on("click", "span", function() {
-  const date = $(this)
-  .text()
-  .trim();
+  const date = $(this).text().trim();
 
-  const dateInput = $("<input>")
-  .attr("type", "text")
-  .addClass("form-control")
-  .val(date);
+  const dateInput = $("<input>").attr("type", "text").addClass("form-control").val(date);
 
   $(this).replaceWith(dateInput);
+
+  dateInput.datepicker({
+    minDate: 1,
+    onClose: function() {
+      $(this).trigger("change");
+    }
+  });
 
   dateInput.select();
 });
 
-$(".list-group").on("blur", "input[type='text']", function() {
-  const date = $(this)
-  .val()
-  .trim();
+$(".list-group").on("change", "input[type='text']", function() {
+  const date = $(this).val().trim();
 
-  const status = $(this)
-  .closest(".list-group")
-  .attr("id")
-  .replace("list-", "");
-
-  const index = $(this)
-  .closest(".list-group-item")
-  .index();
+  const status = $(this).closest(".list-group").attr("id").replace("list-", "");
+  const index = $(this).closest(".list-group-item").index();
 
   tasks[status][index].date = date;
   saveTasks();
 
-  const taskSpan = $("<span>")
-  .addClass("badge badge-primary badge-pill")
-  .text(date);
-
+  const taskSpan = $("<span>").addClass("badge badge-primary badge-pill").text(date);
   $(this).replaceWith(taskSpan);
+
+  auditTask($(taskSpan).closest(".list-group-item"));
 });
 
 // edit task descr
 $(".list-group").on("click", "p", function() {
-  const text = $(this)
-  .text()
-  .trim();
+  const text = $(this).text().trim();
 
-  const textInput = $("<textarea>")
-  .addClass("form-control")
-  .val(text);
+  const textInput = $("<textarea>").addClass("form-control").val(text);
 
   $(this).replaceWith(textInput);
 
@@ -137,26 +140,15 @@ $(".list-group").on("click", "p", function() {
 });
 
 $(".list-group").on("blur", "textarea", function() {
-  const text = $(this)
-  .val()
-  .trim();
+  const text = $(this).val().trim();
   
-  const status = $(this)
-  .closest(".list-group")
-  .attr("id")
-  .replace("list-", "");
-
-  const index = $(this)
-  .closest(".list-group-item")
-  .index();
+  const status = $(this).closest(".list-group").attr("id").replace("list-", "");
+  const index = $(this).closest(".list-group-item").index();
 
   tasks[status][index].text = text;
   saveTasks();
 
-  const taskP = $("<p>")
-  .addClass("m-1")
-  .text(text);
-
+  const taskP = $("<p>").addClass("m-1").text(text);
   $(this).replaceWith(taskP);
 });
 
@@ -171,15 +163,9 @@ $(".card .list-group").sortable({
     const tempArr = [];
     
     $(this).children().each(function() {
-      const text = $(this)
-      .find("p")
-      .text()
-      .trim();
+      const text = $(this).find("p").text().trim();
 
-      const date = $(this)
-      .find("span")
-      .text()
-      .trim();
+      const date = $(this).find("span").text().trim();
 
       tempArr.push({
         text: text,
